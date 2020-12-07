8bb47237260048ed47307a48d20e6dc3ed7d9477
Kenert <kenert2001@gmail.com>
2020-12-07 11:09:29 +0200

    Code commenting and fixed href

--- JS/kartta.js
+++ JS/kartta.js
@@ -3,40 +3,40 @@ let map;
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 
 
-// Asetukset paikkatiedon hakua varten (valinnainen)
+// Settings for location search (optional)
 const options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
 };
 
-// Funktio, joka ajetaan, kun paikkatiedot on haettu
+// Function that is run when location is found
 function success(pos) {
     const crd = pos.coords;
 
-    // Tulostetaan paikkatiedot konsoliin
+    // Print current position to console
     console.log('Your current position is:');
     console.log(`Latitude : ${crd.latitude}`);
     console.log(`Longitude: ${crd.longitude}`);
     console.log(`More or less ${crd.accuracy} meters.`);
 
+    // Creates a new map using gotten coordinates
     createMap(crd.latitude, crd.longitude);
 }
 
-// Funktio, joka ajetaan, jos paikkatietojen hakemisessa tapahtuu virhe
+// Function that is run when there was an error finding a location
 function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
     //If user denied Geolocation, create map on lat 61 and lon 25
     createMap(61,25);
 }
 
-// Käynnistetään paikkatietojen haku
+// Start location search
 navigator.geolocation.getCurrentPosition(success, error, options);
 
+// Function that creates map at specified lat and lon
 function createMap(lat, lon) {
     //Initializing map and setting coordinates and zoom level
-    console.log(lat);
-    console.log(lon);
     map = L.map('map1').setView([lat, lon], 7);
 
     //Adds layer to map
@@ -49,12 +49,13 @@ function createMap(lat, lon) {
         accessToken: 'pk.eyJ1IjoidGhla2VudHMiLCJhIjoiY2todnV6ZTA4MWhvbzJxbGh4ajI0Y2x4ayJ9.7AlWsPXyZBj2IatCVfjnVg'
     }).addTo(map);
 
+    //Gets GeoJSON data and adds them to the map
     geojson = L.geoJson(maakunnat, {
         onEachFeature: onEachFeature
     }).addTo(map);
     geojson.addEventListener("click", onClick);
 }
-//If mouse on layer, highlight
+//If mouse on GeoJSON layer, highlight
 function highlight(event) {
     let layer = event.target;
 
@@ -64,26 +65,21 @@ function highlight(event) {
         dashArray: '',
         fillOpacity: 0.7
     });
-
-    if(!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
-    }
-
 }
-//resets highlight when mouse exits layer
+//Resets highlight when mouse exits GeoJSON layer
 function resetHighlight(event) {
     geojson.resetStyle(event.target);
 }
-function zoomToFeature(event) {
-    map.fitBounds(event.target.getBounds());
-}
+//Tells GeoJSON layers what to do on mouseover and mouseout
 function onEachFeature(feature, layer) {
     layer.on({
         mouseover: highlight,
         mouseout: resetHighlight
     });
 }
+//Function that runs when a layer on the map is clicked
 function onClick(event) {
+    //Gets clicked GeoJSON layers maakunta
     let maakunta = event.sourceTarget.feature.properties.Maakunta;
     const art1 = document.getElementById("art1");
     const new_art = document.createElement("article");
@@ -93,12 +89,14 @@ function onClick(event) {
          </h4>`;
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
+        // If everything is alright on connection, executes
         if (this.readyState == 4 && this.status == 200) {
             new_art.innerHTML = xhttp.responseText;
             console.log(xhttp.responseText);
         }
     };
     art1.replaceWith(new_art);
-    xhttp.open("GET", "http://users.metropolia.fi/~kenertml/haku.php?maakunta="+maakunta, true);
+    //Performs php search
+    xhttp.open("GET", proxy+"http://users.metropolia.fi/~kenertml/haku.php?maakunta="+maakunta, true);
     xhttp.send();
 }
--- JS/tarkempi_haku.js
+++ JS/tarkempi_haku.js
@@ -1,7 +1,8 @@
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 
+//Funtion that runs when haku.html forms submit button is clicked
 function fillInfo(form) {
-    console.log(form.maakunta.value);
+    //Gets form values and assigns them to variables
     let maakunta_haku = form.maakunta.value;
     let merkki_haku = form.merkki.value;
     let malli_haku = form.malli.value;
@@ -14,11 +15,13 @@ function fillInfo(form) {
     tarkka_haku_tulos.setAttribute("id", "tarkka_haku_tulos");
     const xhttp2 = new XMLHttpRequest();
     xhttp2.onreadystatechange = function() {
+        // If everything is alright on connection, executes
         if (this.readyState == 4 && this.status == 200) {
             art3.innerHTML = xhttp2.responseText;
             console.log(xhttp2.responseText);
         }
     };
+    //Performs php search
     xhttp2.open("GET", proxy+"http://users.metropolia.fi/~kenertml/tarkempi_haku.php?maakunta="+maakunta_haku+"&merkki="+merkki_haku+"&malli="+malli_haku+"&vari="+vari_haku, true);
     xhttp2.send();
 }
